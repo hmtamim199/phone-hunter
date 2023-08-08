38b95bf2902c03108e1c732d7a846e229295b635
hmtamim199 <hmtamim8906@gmail.com>
2023-08-08 17:01:59 +0600

    added cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,9 +6,12 @@ const Cart = ({ cart }) => {
 
   let total = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    product.quantity = product.quantity || 1;
+    total = total + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
 
   const tax = total * 7 / 100;
@@ -17,7 +20,7 @@ const Cart = ({ cart }) => {
   return (
     <div className='cart'>
       <h3>card is here</h3>
-      <p>selected items:{cart.length}</p>
+      <p>selected items:{quantity}</p>
       <p>Total price:${total}</p>
       <p> shipping:${totalShipping.toFixed(2)}</p>
       <p>tax:${tax.toFixed(2)}</p>
